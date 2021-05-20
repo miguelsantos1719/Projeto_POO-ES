b3f9e467cd5f064e261dd4af58bd8b9b47e8f05e
miguelsantos1719 <79711981+miguelsantos1719@users.noreply.github.com>
2021-05-20 15:00:58 +0100

    Add files via upload

---
+++ Jogo_Rally/Troço.java
@@ -0,0 +1,17 @@
+package Jogo_Rally;
+
+public class Troço {
+	//1.ATRIBUTOS
+
+	//2.ACESSORES
+
+	//3.CONSTRUTOR - default
+
+	//4.CONSTRUTOR - com parâmetros
+
+	//5.CONSTRUTOR - Cópia
+
+	//6.COMPORTAMENTOS
+
+	//7.MÉTODOS COMPLEMENTARES
+}
